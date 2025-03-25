cb6c14800303fb4addd35884db4c6133a3b3ca90
Emeka-techDev <gilbertgenye@gmail.com>
2025-03-25 12:36:19 +0100

    image upload

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -9,10 +9,20 @@ use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use App\Filament\Clusters\Settings;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\CategoryResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\CategoryResource\RelationManagers;
+
+
+
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+// use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 
 class CategoryResource extends Resource
 {
@@ -29,9 +39,13 @@ class CategoryResource extends Resource
                 Forms\Components\TextInput::make('name')
                     ->label('category_name')
                     ->required(),
-            
-                Forms\Components\TextInput::make('image_url')
+
+                FileUpload::make('image_url')
+                    ->avatar()
                     ->label('image_url'),
+            
+                // Forms\Components\TextInput::make('image_url')
+                //     ->label('image_url'),
             ]);
     }
 
@@ -39,12 +53,30 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
+                ImageColumn::make('image_url')
+                    ->circular(),
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\TextColumn::make('image_url'),
                 Tables\Columns\TextColumn::make('products_count') // Use products_count
                     ->label('Product Quantity')
                     ->getStateUsing(fn (Category $record) => $record->productCount()) // Call the function
                     ->sortable(),
+
+
+                // Split::make([
+                //     ImageColumn::make('image_url')
+                //         ->circular(),
+                //     TextColumn::make('name')
+                //         ->weight(FontWeight::Bold)
+                //         ->searchable()
+                //         ->sortable(),
+                //     Stack::make([
+                //         TextColumn::make('phone')
+                //             ->icon('heroicon-m-phone'),
+                //         TextColumn::make('email')
+                //             ->icon('heroicon-m-envelope'),
+                //     ]),
+                // ])
             ])
             ->filters([
                 //
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -39,6 +39,7 @@ class Product extends Model
         'size',
         'description',
         'quantity',
+        'logo',
         'price'
     ];
 
