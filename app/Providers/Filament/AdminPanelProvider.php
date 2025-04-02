b9fd6faca4b65c6e4b26191498047a7335fa7efd
Emeka-techDev <gilbertgenye@gmail.com>
2025-04-02 13:52:19 +0100

    removed filament widgets and included logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
                 // 'success' => Color::Green,
                 // 'warning' => Color::Orange,
             ])
-            //  ->defaultThemeMode(ThemeMode::Dark) 
+            ->brandLogo(asset('images/logo.jpg')) 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
