1691e2de202ae31524b8a4b8172854976a05bf97
Emeka-techDev <gilbertgenye@gmail.com>
2025-04-02 16:12:52 +0100

    forget password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->registration(Register::class) 
+            ->login()
             ->passwordReset()
             ->emailVerification()
             ->colors([
